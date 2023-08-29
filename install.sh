fcc9f33f65b686dda3139b41782a08534be20332
mesumine <mesumine@gmail.com>
2023-08-29 13:17:51 -0400

    stupid dots..

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sudo apt install -y i3-wm i3blocks i3lock-fancy i3status xfce4-terminal rofi feh picom flameshot arandr
-cp -r .config/* ~/.config/
+cp -r config/* ~/.config/
 mv ~/.zshrc ~/zshrc.bak
-mv .zshrc ~/.zshrc 
+mv zshrc ~/.zshrc 
 
 
 
