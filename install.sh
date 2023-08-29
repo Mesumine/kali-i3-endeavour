0f81d4272bd3c3e19f9222f037d80d62ffe99238
mesumine <mesumine@gmail.com>
2023-08-29 13:36:08 -0400

    all config files

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo apt install -y i3-wm i3blocks i3lock-fancy i3status xfce4-terminal rofi feh picom flameshot arandr
 
-rm -rf ~/.config
+rm -rf ~/.config/i3/
 cp -r config/* ~/.config/
 
 chmod +x ~/.config/i3/scripts/*
