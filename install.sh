0b3e2e60f47b43005a6b1221d237cdec96bded93
mesumine <mesumine@gmail.com>
2023-08-29 13:28:19 -0400

    i3 configs.....

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 sudo apt install -y i3-wm i3blocks i3lock-fancy i3status xfce4-terminal rofi feh picom flameshot arandr
+
+rm -rf ~/.config
 cp -r config/* ~/.config/
+
+chmod +x ~/.config/i3/scripts/*
 mv ~/.zshrc ~/zshrc.bak
 mv zshrc ~/.zshrc 
 
