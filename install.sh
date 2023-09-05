6652be84ea7fc7c5880dab0c75e1c609ff551e1a
mesumine <mesumine@gmail.com>
2023-09-04 23:44:46 -0400

    ubuntu

--- install.sh
+++ install.sh
@@ -64,7 +64,7 @@ while getopts "habc:mvw" opt; do
 done
 
 ## update 
-sudo apt update 
+sudo apt update 1>/dev/null 
 ## Install packages
 echo "Installing the following packages:"
 for item in "${selection[@]}";do 
@@ -78,6 +78,18 @@ uname -a | grep parrot > /dev/null
 if [ $? -eq 0 ]; then
     echo -e "\nParrot detected, installing the following additional packages:\ni3-gaps\nzsh\nzsh-autosuggestions\nzsh-syntax-highlighting"
     sudo apt-get install -qq -y zsh i3-gaps zsh-autosuggestions zsh-syntax-highlighting | 1>/dev/null
+    chsh -s $(which zsh) $(whoami)
+    sudo chsh -s $(which zsh) root  
+fi 
+# ubuntu specific configuration 
+uname -a | grep ubuntu > /dev/null 
+if [ $? -eq 0 ]; then
+    echo -e "\nUbuntu detected, installing the following additional packages:\ni3 (from ppa)\nzsh\nzsh-autosuggestions\nzsh-syntax-highlighting\npython3-pip\nfonts-font-awesome"
+    /usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2023.02.18_all.deb keyring.deb SHA256:a511ac5f10cd811f8a4ca44d665f2fa1add7a9f09bef238cdfad8461f5239cc4
+    sudo apt install ./keyring.deb
+    echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" | sudo tee /etc/apt/sources.list.d/sur5r-i3.list 1>/dev/null 
+    sudo apt update 1>/dev/null 
+    sudo apt-get install -qq -y zsh i3 zsh-autosuggestions zsh-syntax-highlighting fonts-font-awesome python3-pip | 1>/dev/null
     chsh -s $(which zsh) $(whoami)
     sudo chsh -s $(which zsh) root  
 fi 
